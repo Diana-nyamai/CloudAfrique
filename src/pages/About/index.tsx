7d5f5ef03f4b73f5715c59804edaee84b7327534
Diana-nyamai <dnyamai.dn@gmail.com>
2024-01-20 13:24:06 +0300

    Update about hero

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,7 +2,14 @@
 const About = () => {
   return (
     <div>
-      this is about page
+     
+     <div className='relative bg-blue h-full flex flex-col lg:flex-row justify-center items-center lg:px-32 pb-32'>
+        <div className='text-white px-5 xl:w-1/2 mt-10'>
+          <h1 className='text-white text-2xl lg:text-6xl capitalize mb-4 font-playfair text-center'>About <span className="text-orange">Us</span> </h1>
+          <p className="font-montserrat mb-4 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel dolorum laudantium nesciunt exercitationem modi. In sit saepe eius, mollitia hic voluptatem eligendi ipsam recusandae, accusamus assumenda, maxime optio aliquam nostrum!</p>
+          
+        </div>
+    </div>
     </div>
   )
 }
